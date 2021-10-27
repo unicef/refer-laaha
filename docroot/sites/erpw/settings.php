91ba0c278c1a6e454afac807e0b57817a8cee214
Talish Khan <talish.khan@srijan.net>
2021-10-27 17:10:28 +0530

    updated settings.php

--- docroot/sites/erpw/settings.php
+++ docroot/sites/erpw/settings.php
@@ -770,9 +770,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
-}
+#if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+ # include $app_root . '/' . $site_path . '/settings.local.php';
+#}
 
 
 // On Acquia Cloud, this include file configures Drupal to use the correct
