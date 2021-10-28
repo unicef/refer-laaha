961495c34bb7de39c5e83c342afaa67ba00a2ab9
Talish Khan <talish.khan@srijan.net>
2021-10-28 16:17:57 +0530

    Config Split setup

---
+++ docroot/sites/default/settings.dev.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Settings for dev environment.
+ */
+
+// Config split for dev.
+$config['config_split.config_split.dev']['status'] = TRUE;
+$config['environment_indicator.indicator']['bg_color'] = '#8fce00';
+$config['environment_indicator.indicator']['fg_color'] = '#FFFFFF';
+$config['environment_indicator.indicator']['name'] = 'Acquia Dev';
---
+++ docroot/sites/default/settings.test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Settings for stage environment.
+ */
+
+// Config split for stage.
+$config['config_split.config_split.stage']['status'] = TRUE;
+$config['environment_indicator.indicator']['bg_color'] = '#2986cc';
+$config['environment_indicator.indicator']['fg_color'] = '#FFFFFF';
+$config['environment_indicator.indicator']['name'] = 'Acquia Stage';
