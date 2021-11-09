69057a339707158253f4c039d006ff30d6ed1635
Talish Khan <talish.khan@srijan.net>
2021-11-09 12:06:57 +0530

    Fixing multilingual issue

--- docroot/sites/default/settings.dev.php
+++ docroot/sites/default/settings.dev.php
@@ -7,6 +7,7 @@
 
 // Config split for dev.
 $config['config_split.config_split.dev']['status'] = TRUE;
+$config['config_split.config_split.local']['status'] = FALSE;
 $config['environment_indicator.indicator']['bg_color'] = '#f44336';
 $config['environment_indicator.indicator']['fg_color'] = '#FFFFFF';
 $config['environment_indicator.indicator']['name'] = 'Acquia Dev';
