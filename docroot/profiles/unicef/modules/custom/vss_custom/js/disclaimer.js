a8f975d33c6914fc308c2a9d092dec932c1ff66c
srijan-dj <divij.b@srijan.net>
2021-11-16 14:20:06 +0530

    Updating narration files

--- docroot/profiles/unicef/modules/custom/vss_custom/js/disclaimer.js
+++ docroot/profiles/unicef/modules/custom/vss_custom/js/disclaimer.js
@@ -17,55 +17,44 @@
         return;
         }
         window.speechSynthesis.cancel();
+
         // grab the UI elements to work with
-        const playEl = document.getElementById('play');
-        const pauseEl = document.getElementById('pause');
+        const play = document.getElementById('play');
+        const pause = document.getElementById('pause');
+        const resume = document.getElementById('resume');
+        resume.hidden = true;
+        pause.hidden = true;
         // add UI event handlers
-        playEl.addEventListener('click', play);
-        pauseEl.addEventListener('click', pause);
+        play.addEventListener("click", () => {
+          // data.innerText = "play";
+          var utterance = new SpeechSynthesisUtterance(drupalSettings.disclaimer_narrate);
+          utterance.lang = "en-US";
+          play.hidden = true;
+          resume.hidden = true;
+          pause.hidden = false;
+          speechSynthesis.speak(utterance);
+          utterance.addEventListener('end', function (event) {
+            speechSynthesis.cancel();
+            play.hidden = false;
+            resume.hidden = true;
+            pause.hidden = true;
+          });
+        });
 
-        // set text
-        text = drupalSettings.disclaimer_narrate;
+        pause.addEventListener("click", () => {
+          speechSynthesis.pause();
+          play.hidden = true;
+          pause.hidden = true;
+          resume.hidden = false;
+        });
 
-
-        function play(event) {
-          if (sessionStorage.getItem('id') != 'play') {
-            sessionStorage.setItem('id', 'play');
-            window.speechSynthesis.cancel();
-          }
-          if (window.speechSynthesis.speaking) {
-            // there's an unfinished utterance
-            window.speechSynthesis.resume();
-          }
-          else {
-            // start new utterance
-            const utterance = new SpeechSynthesisUtterance(text);
-            utterance.addEventListener('start', handleStart);
-            utterance.addEventListener('pause', handlePause);
-            utterance.addEventListener('resume', handleResume);
-            utterance.lang = drupalSettings.disclaimer_landId;
-            window.speechSynthesis.speak(utterance);
-          }
-        }
-
-        function pause() {
-          window.speechSynthesis.pause();
-        }
-        function handleStart() {
-          playEl.disabled = true;
-          pauseEl.disabled = false;
-        }
-
-        function handlePause() {
-          playEl.disabled = false;
-          pauseEl.disabled = true;
-        }
-
-        function handleResume() {
-          playEl.disabled = true;
-          pauseEl.disabled = false;
-        }
-
+        resume.addEventListener("click", () => {
+          // data.innerText = "resume";
+          speechSynthesis.resume();
+          play.hidden = true;
+          resume.hidden = true;
+          pause.hidden = false;
+        });
       });
     }
   };
