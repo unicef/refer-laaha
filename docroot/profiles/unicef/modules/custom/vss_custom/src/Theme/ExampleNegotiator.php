1a52dd1570695be4beaeaa8c6ae810a4dcbd7807
Adil Siddiqui <adil.siddiqui@srijan.net>
2021-11-18 20:39:30 +0530

    [NCF-517 POC - Switch AMP theme for category pages]

---
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/Theme/ExampleNegotiator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\vss_custom\Theme;
+
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * Defines a service that is use for taxonomy theme switch.
+ */
+class ExampleNegotiator implements ThemeNegotiatorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    // Use this theme on a certain route.
+    return $route_match->getRouteName() == 'entity.taxonomy_term.canonical';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    if ($_SERVER['QUERY_STRING'] == 'amp') {
+      return 'vss_amp';
+    }
+  }
+
+}
