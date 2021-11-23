eed34026f6ee4351c0d5a52fba590ea282d4f15c
Talish Khan <talish.khan@srijan.net>
2021-11-23 15:07:09 +0530

    [NCF-407] Video transcript

---
+++ docroot/profiles/unicef/themes/custom/vss/js/transcript.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * JavaScript for Transcript.
+ */
+
+ (function ($, Drupal, drupalSettings) {
+    Drupal.behaviors.transcriptJs = {
+      attach: function (context, settings) {
+        $(document).ready(function () {
+         
+  
+        });
+      }
+    };
+  
+  })(jQuery, Drupal, drupalSettings);
