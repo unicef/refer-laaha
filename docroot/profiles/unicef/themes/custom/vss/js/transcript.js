7d5359b21ed93f320e28d5a9705b46292396723a
Talish Khan <talish.khan@srijan.net>
2021-11-24 12:50:48 +0530

    Updated transcript

--- docroot/profiles/unicef/themes/custom/vss/js/transcript.js
+++ docroot/profiles/unicef/themes/custom/vss/js/transcript.js
@@ -3,19 +3,51 @@
  * JavaScript for Transcript.
  */
 
- (function ($, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings) {
     Drupal.behaviors.transcriptJs = {
-      attach: function (context, settings) {
-        $(document).ready(function () {
-            $(document).on('click', '.btnSeek', function() {
-                var seekToTime = $(this).data('seek');
+        attach: function (context, settings) {
+            $(document).ready(function () {
+                $(document).on('click', '.btnSeek', function () {
+                    var seekToTime = $(this).data('seek');
+                    var player = $('#video').get(0);
+                    player.currentTime = seekToTime;
+                    player.play();
+                });
+                // Highlight the transcript based on the video time.
+                var lines = document.getElementById("transcript").getElementsByTagName("div");
                 var player = $('#video').get(0);
-                player.currentTime = seekToTime;
-                player.play();
+
+                // Update the progress bar as the video plays
+                player.addEventListener('timeupdate', function () {
+                    var now = player.currentTime;
+
+                    // highlight text as video plays
+                    for (var i = 0, l = lines.length; i < l; i++) {
+                        if (now >= lines[i].getAttribute("data-start") &&
+                            now <= lines[i].getAttribute("data-end")) {
+                            lineId = lines[i].getAttribute('id');
+                            $("#" + lineId + " > .transcript-text").addClass("current");
+                        }
+                        else {
+                            lineId = lines[i].getAttribute('id');
+                            $("#" + lineId + " > .transcript-text").removeClass("current");
+                        }
+                    }
+                });
+                // Toggle Transcript.
+                $(".show-transcript").click(function(){
+                    $(".hide-transcript").show();
+                    $(".show-transcript").hide();
+                    $("#transcript").show();      
+                });
+                $(".hide-transcript").click(function(){
+                    $(".show-transcript").show();
+                    $(".hide-transcript").hide();
+                    $("#transcript").hide();  
+                });
+
             });
-  
-        });
-      }
+        }
     };
-  
-  })(jQuery, Drupal, drupalSettings);
+
+})(jQuery, Drupal, drupalSettings);
