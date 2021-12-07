d49a71bf6755384e254a0ea2cff8aacd8dca34b3
srijan-dj <divij.b@srijan.net>
2021-12-07 20:17:25 +0530

    changing path of ajax calls.

--- docroot/profiles/unicef/modules/custom/vss_custom/js/related_content.js
+++ docroot/profiles/unicef/modules/custom/vss_custom/js/related_content.js
@@ -3,7 +3,7 @@
   'use strict';
   $(document).ready(function () {
     let r = (Math.random() + 1).toString(36).substring(7);
-    Drupal.ajax({ url: '/related-content/9?q=' + r }).execute();
+    Drupal.ajax({ url: '/related-content/' + drupalSettings.path.currentPath+'?q=' + r }).execute();
   });
 
 })(jQuery, Drupal, drupalSettings);
