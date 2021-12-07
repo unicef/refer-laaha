aebca009225cdbd370d16f9c04ae6413682dc69b
Talish Khan <talish.khan@srijan.net>
2021-12-07 15:42:06 +0530

    Updated settings and pipeline for eRPW

--- docroot/sites/erpw/settings.dev.php
+++ docroot/sites/erpw/settings.dev.php
@@ -10,4 +10,4 @@ $config['config_split.config_split.dev']['status'] = TRUE;
 $config['config_split.config_split.local']['status'] = FALSE;
 $config['environment_indicator.indicator']['bg_color'] = '#6aa84f';
 $config['environment_indicator.indicator']['fg_color'] = '#FFFFFF';
-$config['environment_indicator.indicator']['name'] = 'Acquia Dev';
+$config['environment_indicator.indicator']['name'] = 'eRPW Development';
