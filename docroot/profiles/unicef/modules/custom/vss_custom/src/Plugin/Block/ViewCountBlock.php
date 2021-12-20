3f212da0be2ef6bc24c46a639ba0fbd27320625f
Adil Siddiqui <adil.siddiqui@srijan.net>
2021-12-20 20:01:40 +0530

    NCF-547 Save Node View Count

---
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/ViewCountBlock.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\vss_custom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'SocialIconsFooterBlock' block.
+ *
+ * @Block(
+ *  id = "view_count_block",
+ *  admin_label = @Translation("View Count block"),
+ * )
+ */
+class ViewCountBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Language\LanguageManagerInterface.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->pageCacheKillSwitch = $container->get('page_cache_kill_switch');
+    $instance->routeMatch = $container->get('current_route_match');
+    $instance->connection = $container->get('database');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    if ($this->routeMatch->getRouteName() == 'entity.node.canonical') {
+      $nid = $this->routeMatch->getRawParameter('node');
+    }
+    $this->pageCacheKillSwitch->trigger();
+    $view_count = $this->getViewCount($nid);
+    $build['#theme'] = 'view_count_block';
+    $build['#content'] = $view_count + 1;
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewCount($nid) {
+    $query = $this->connection->select('nodeviewcount', 'nvc');
+    $query->condition('nvc.nid', $nid);
+    $query->addExpression("COUNT(nvc.nid)", "nvc");
+    $node = $query->execute()->fetchField();
+    return $node;
+  }
+
+}
