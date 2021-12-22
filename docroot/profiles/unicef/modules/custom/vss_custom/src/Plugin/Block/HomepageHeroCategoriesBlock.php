14f7a25cd7c506ce41bdbab57122db0be5f83237
aziza-anwari <aziza.anwari@srijan.net>
2021-12-22 14:20:27 +0530

    fixing indentation issues

--- docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/HomepageHeroCategoriesBlock.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/HomepageHeroCategoriesBlock.php
@@ -7,7 +7,6 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Cache\Cache;
 use Drupal\taxonomy\Entity\Term;
-Use Drupal\Core\Url;
 
 /**
  * Provides a 'SocialIconsFooterBlock' block.
@@ -52,26 +51,25 @@ class HomepageHeroCategoriesBlock extends BlockBase implements ContainerFactoryP
     $content = $this->vssCommonConfigDefault->getCategories();
 
     foreach ($content['homepage_hero'] as $term_id) {
-          $term_obj =  Term::load($term_id);
-          $term_name = $term_obj->getName();
-          $term_bgcolor = $term_obj->get('field_category_color')->getValue()[0]['color'];
-          $icon_path = $term_obj->get('field_icon')->entity->getFileUri();
-          $term_alias = $term_obj->get('path')->alias;
-          $hero[] = array(
-            'id' => $term_id,
-            'name' => $term_name,
-            'color' => $term_bgcolor,
-            'cat_uri' => $term_alias,
-            'icon' => $icon_path,
-          );
+      $term_obj = Term::load($term_id);
+      $term_name = $term_obj->getName();
+      $term_bgcolor = $term_obj->get('field_category_color')->getValue()[0]['color'];
+      $icon_path = $term_obj->get('field_icon')->entity->getFileUri();
+      $term_alias = $term_obj->get('path')->alias;
+      $hero[] = [
+        'id' => $term_id,
+        'name' => $term_name,
+        'color' => $term_bgcolor,
+        'cat_uri' => $term_alias,
+        'icon' => $icon_path,
+      ];
     }
     $build['#theme'] = 'homepage_hero_categories_block';
     $build['#content'] = $hero;
     $build['#lang_code'] = $langcode;
     $build['#cache']['tags'] = $this->getCacheTags();
-    $build['#cache']['contexts'] = $this->getCacheContexts(); 
-    
-    
+    $build['#cache']['contexts'] = $this->getCacheContexts();
+
     return $build;
   }
 
