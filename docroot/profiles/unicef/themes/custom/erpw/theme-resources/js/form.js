6203b7a065e54a37b192accc2fe976bf71d274f8
Preeti.chawla <preeti.chawla@outlook.com>
2021-12-27 12:16:37 +0530

    NCF-602 and 603: Menu logged and non logged user.

--- docroot/profiles/unicef/themes/custom/erpw/theme-resources/js/form.js
+++ docroot/profiles/unicef/themes/custom/erpw/theme-resources/js/form.js
@@ -1,15 +1,17 @@
 (function ($, Drupal, drupalSettings) {
   'use strict';
-    if($( "select option:selected").val() == 0) {
-      $('select').css('color', '#E0D7D7');
-    };
-    $('select').on('change', function() {
-      if ($(this).children("option:selected").val() == 0) {
-      return $(this).css('color', '#E0D7D7');
-        } else {
-      return $(this).css('color', '#444');
-      }
-      
-    });
-   
-})(jQuery, Drupal, drupalSettings);
+  Drupal.behaviors.form_select = {
+    attach: function (context, settings) {
+      if($( "select option:selected").val() == 0) {
+        $('select').css('color', '#E0D7D7');
+      };
+      $('select').on('change', function() {
+        if ($(this).children("option:selected").val() == 0) {
+        return $(this).css('color', '#E0D7D7');
+          } else {
+        return $(this).css('color', '#444');
+        }
+      });
+    }
+  };
+}(jQuery, Drupal, drupalSettings));
