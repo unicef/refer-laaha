243297eedbec597cb4a9343ecbfa7cb368d67df2
Talish Khan <talish.khan@srijan.net>
2021-12-27 16:32:46 +0530

    Fixing sign up form for erpw

--- docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/SignUpForm.php
+++ docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/SignUpForm.php
@@ -177,6 +177,7 @@ class SignUpForm extends FormBase {
       '#submit' => ['::submitPageOne'],
       '#validate' => ['::validatePageOne'],
     ];
+    $form['#cache']['max-age'] = 0;
     return $form;
   }
 
@@ -269,6 +270,7 @@ class SignUpForm extends FormBase {
         'event' => 'click',
       ],
     ];
+    $form['#cache']['max-age'] = 0;
     $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
     $form['#attached']['library'][] = 'erpw_custom/erpw_js';
     return $form;
