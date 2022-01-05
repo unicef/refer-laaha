f048104c1125e1a0645fa45fc6e4ca420f2a95ac
Adil Siddiqui <adil.siddiqui@srijan.net>
2022-01-05 20:58:16 +0530

    BE - Dashboard of reports - Most Viewed Content Report

---
+++ docroot/profiles/unicef/modules/custom/visitors/src/Form/DateFilter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\visitors\Form;
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Datetime\DateHelper;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Date Filter.
+ */
+class DateFilter extends FormBase {
+
+  /**
+   * Form Id.
+   */
+  public function getFormID() {
+    return 'visitors_date_filter_form';
+  }
+
+  /**
+   * Form builder
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $from = \Drupal::request()->query->get('timestamp');
+    $form = [];
+
+    $form['visitors_date_from_filter'] = [
+      '#title'            => t('Timestamp'),
+      '#type'             => 'select',
+      '#default_value'    => $from,
+      '#options'          => [
+        '' => t('Today'),
+        'cur_week' => t('This Week'),
+        'cur_mon' => t('This Month'),
+        'cur_yr' => t('This Year'),
+      ]
+    ];
+
+    $form['submit'] = [
+      '#type'             => 'submit',
+      '#value'            => t('Apply'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * validate form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $fromvalue     = $form_state->getValue('visitors_date_from_filter');
+    $tovalue       = $form_state->getValue('to');
+    $from          = [];
+    $to            = [];
+  }
+
+  /**
+   * Submit form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $from = $form_state->getValue('visitors_date_from_filter');
+    $url = Url::fromRoute('visitors.top_pages', [], ['query' => ['timestamp' => $from]]);
+    $form_state->setRedirectUrl($url);
+  }
+}
