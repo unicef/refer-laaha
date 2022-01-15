9528ba2293089cdd73229683654ef7d6646f972f
Adil Siddiqui <adil.siddiqui@srijan.net>
2022-01-15 22:17:20 +0530

    category changes

--- docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/HeroBannerBlock.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/HeroBannerBlock.php
@@ -50,52 +50,34 @@ class HeroBannerBlock extends BlockBase implements ContainerFactoryPluginInterfa
     $build = [];
     if ($this->routeMatch->getRouteName() == 'entity.taxonomy_term.canonical') {
       $term_id = $this->routeMatch->getRawParameter('taxonomy_term');
-    }
-    if ($this->routeMatch->getRouteName() == 'entity.node.canonical') {
-      $nid = $this->routeMatch->getRawParameter('node');
-      $node = $this->entityTypeManager->getStorage('node')->load($nid);
-      $child_tid = $node->get('field_sub_category')->target_id;
-      $parent = $this->entityTypeManager->getStorage('taxonomy_term')->loadParents($child_tid);
-      $parent = reset($parent);
-      $term_id = $parent->id();
-    }
+      $langcode = $this->languageManager->getCurrentLanguage()->getId();
+      $vid = 'categories';
+      $parent_tid = $term_id;
+      $depth = 1;
+      $load_entities = TRUE;
+      $child_terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid, $parent_tid, $depth, $load_entities);
+      $subcat_details = [];
+      $cat_details = [];
 
-    $langcode = $this->languageManager->getCurrentLanguage()->getId();
-    $vid = 'categories';
-    $parent_tid = $term_id;
-    $depth = 1;
-    $load_entities = TRUE;
-    $child_terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid, $parent_tid, $depth, $load_entities);
-    $subcat_details = [];
-    $cat_details = [];
-
-    if ($child_terms) {
-      $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($term_id);
-      if ($term->hasTranslation($langcode)) {
-        $term = $term->getTranslation($langcode);
-      }
-      $name = $term->label();
-      $cat_color = $term->get('field_category_color')->color;
-      $cat_icon = $term->get('field_icon')->target_id;
-      $file = $this->entityTypeManager->getStorage('file')->load($cat_icon);
-      if ($file) {
-        $cat_ic = $file->getFileUri();
-        $cat_ic = str_replace('public://', 'sites/default/files/', $cat_ic);
-      }
-      foreach ($child_terms as $k => $child) {
-        if ($this->routeMatch->getRouteName() == 'entity.node.canonical' && ($child_tid == $child->get('tid')->value)) {
-          unset($child_terms[$k]);
-          $cat_details['cat_name'] = '';
-          $cat_details['cat_icon'] = '';
+      if ($child_terms) {
+        $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($term_id);
+        if ($term->hasTranslation($langcode)) {
+          $term = $term->getTranslation($langcode);
         }
-        else {
+        $name = $term->label();
+        $cat_color = $term->get('field_category_color')->color;
+        $cat_icon = $term->get('field_icon')->target_id;
+        $file = $this->entityTypeManager->getStorage('file')->load($cat_icon);
+        if ($file) {
+          $cat_ic = $file->getFileUri();
+          $cat_ic = str_replace('public://', 'sites/default/files/', $cat_ic);
+        }
+        foreach ($child_terms as $k => $child) {
           if ($child->hasTranslation($langcode)) {
             $child = $child->getTranslation($langcode);
           }
-          if ($this->routeMatch->getRouteName() != 'entity.node.canonical') {
-            $cat_details['cat_name'] = $name;
-            $cat_details['cat_icon'] = $cat_ic;
-          }
+          $cat_details['cat_name'] = $name;
+          $cat_details['cat_icon'] = $cat_ic;
           $cat_details['cat_color'] = $cat_color;
           $subcat_details[$k]['subcat_name'] = $child->get('name')->value;
           $file = $this->entityTypeManager->getStorage('file')->load($child->get('field_sub_category_thumbnail')->target_id);
@@ -107,7 +89,48 @@ class HeroBannerBlock extends BlockBase implements ContainerFactoryPluginInterfa
         }
       }
     }
+    if ($this->routeMatch->getRouteName() == 'entity.node.canonical') {
+      $nid = $this->routeMatch->getRawParameter('node');
+      $node = $this->entityTypeManager->getStorage('node')->load($nid);
+      foreach ($node->field_sub_category->getValue() as $value) {
+        if (!empty($value['target_id'])) {
+          $parent = $this->entityTypeManager->getStorage('taxonomy_term')->loadParents($value['target_id']);
+          $parent = reset($parent);
+          $term_id = $parent->id();
+          $parent_id[$term_id] = $term_id;
+          $langcode = $this->languageManager->getCurrentLanguage()->getId();
+          $vid = 'categories';
+          $parent_tid = $term_id;
+          $depth = 1;
+          $load_entities = TRUE;
+          $arr[$value['target_id']] = $value['target_id'];
 
+        }
+      }
+
+      foreach ($parent_id as $val) {
+        $child_terms1 = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid, $val, $depth, $load_entities);
+        $cat_details['cat_name'] = '';
+        $cat_details['cat_icon'] = '';
+        foreach ($child_terms1 as $child) {
+          if ($child->hasTranslation($langcode)) {
+            $child = $child->getTranslation($langcode);
+          }
+          if ((!in_array($child->get('tid')->value, $arr))) {
+            $subcat_tid = $child->get('tid')->value;
+            $subcat_details[$subcat_tid]['subcat_name'] = $child->get('name')->value;
+            $file = $this->entityTypeManager->getStorage('file')->load($child->get('field_sub_category_thumbnail')->target_id);
+            if ($file) {
+              $file_url = $file->getFileUri();
+              $subcat_details[$subcat_tid]['sub_category_thumbnail'] = str_replace('public://', 'sites/default/files/', $file_url);
+            }
+            $subcat_details[$subcat_tid]['url'] = ltrim($this->aliaspath->getAliasByPath('/taxonomy/term/' . $child->get('tid')->value), '/');
+          }
+        }
+      }
+
+    }
+    // dump($subcat_details);
     $build['#theme'] = 'hero_banner_block';
     $build['#content'] = $subcat_details;
     $build['#category'] = $cat_details;
