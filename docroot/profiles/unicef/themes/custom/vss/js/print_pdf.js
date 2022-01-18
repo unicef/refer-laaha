069e6805ea857578699b834553d9d977773b7538
aziza-anwari <aziza.anwari@srijan.net>
2022-01-18 08:26:14 +0530

    print pdf

--- docroot/profiles/unicef/themes/custom/vss/js/print_pdf.js
+++ docroot/profiles/unicef/themes/custom/vss/js/print_pdf.js
@@ -1,24 +1,20 @@
 (function ($, Drupal, drupalSettings) {
     'use strict';
-  
-    $(document).ready(function(){
-        console.log('fkdksdfgmk');
+        $(".export-to-pdf").click(function(){
+            generatePDF();
+        })
         function generatePDF() {
             // Choose the element that our invoice is rendered in.
-            const element = document.getElementById('pdf-print-nodes');
-            alert(element);
-            // Choose the element and save the PDF for our user.
+            const element = document.getElementById('pdf-content');
             var opt = {
                 margin:       1,
                 filename:     'myfile.pdf',
                 image:        { type: 'jpeg', quality: 0.98 },
                 html2canvas:  { scale: 2 },
-                jsPDF:        { unit: 'in', format: 'letter', orientation: 'landsacpe' }
-                };
-            html2pdf().from(element).save();
+                jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
+              };
+              
+              // New Promise-based usage:
+              html2pdf(element,opt);
         }
-    });
-  
-  
-  
   })(jQuery, Drupal, drupalSettings);
