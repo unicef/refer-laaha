f0f5d94816c21dcf31a4c83930d9e34388542957
himmat1012 <himmat.bhatia@srijan.net>
2022-01-24 09:44:23 +0530

    NCF-609-Update-location-details

--- docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/AddLocationPopup.php
+++ docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/AddLocationPopup.php
@@ -22,12 +22,12 @@ class AddLocationPopup extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state, string $header_message = NULL, string $message = NULL) {
     $url = Url::fromRoute('erpw_custom.dashboard');
     $external_link = Link::fromTextAndUrl('BACK TO DASHBOARD', $url)->toString();
     $form['message'] = [
       '#type' => 'markup',
-      '#markup' => '<div class="review-msg">' . $this->t('Location added') . '</div><div class="email-notify">' . $this->t('The location has been added successfully. You can now access it in the application') . '</div>',
+      '#markup' => '<div class="review-msg">' . $header_message . '</div><div class="email-notify">' . $message . '</div>',
     ];
     $form['back_to_dashboard'] = [
       '#type' => 'markup',
