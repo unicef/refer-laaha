93a439484233846d751bb8d3b9d8730922a3c3f5
aziza-anwari <aziza.anwari@srijan.net>
2022-01-27 22:48:44 +0530

    narrate js

--- docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/LocationSelectorBlock.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/LocationSelectorBlock.php
@@ -84,11 +84,9 @@ class LocationSelectorBlock extends BlockBase implements ContainerFactoryPluginI
       $voiceId = 41;
       $build['#attached']['drupalSettings']['voiceId'] = $voiceId;
     }
-    $build['#attached']['drupalSettings']['disclaimer_landId'] = $lang_id;
-    $build['#attached']['drupalSettings']['disclaimer_narrate'] = $string;
-    $build['#attached']['drupalSettings']['disclaimer'] = TRUE;
-    $build['#cache']['tags'] = $this->getCacheTags();
-    $build['#cache']['contexts'] = $this->getCacheContexts();
+    $build['#attached']['drupalSettings']['location_selector_landId'] = $lang_id;
+    $build['#attached']['drupalSettings']['location_selector_narrate'] = $string;
+    $build['#attached']['drupalSettings']['location_selector'] = TRUE;
     $this->pageCacheKillSwitch->trigger();
     return $build;
   }
