50fbd5a48cc8b0b69f8d15b0d1ef3b5ce8922d93
sager.patel <sagar.patel@srijan.net>
2022-02-01 18:15:32 +0530

    Updated code with t() function.

--- docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/Dashboard.php
+++ docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/Dashboard.php
@@ -24,7 +24,7 @@ class Dashboard extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, $options = NULL) {
     $url = Url::fromRoute('erpw_location.manage_location');
-    $external_link = Link::fromTextAndUrl('Manage locations', $url)->toString();
+    $external_link = Link::fromTextAndUrl($this->t('Manage locations'), $url)->toString();
     $form['manage_location'] = [
       '#type' => 'markup',
       '#prefix' => '<div class="dashboard-link location-link">',
@@ -32,7 +32,7 @@ class Dashboard extends FormBase {
       '#suffix' => '</div>',
     ];
     $url = Url::fromRoute('view.manage_service_types.page_1');
-    $link = Link::fromTextAndUrl('Manage service types', $url)->toString();
+    $link = Link::fromTextAndUrl($this->t('Manage service types'), $url)->toString();
     $form['manage_services'] = [
       '#type' => 'markup',
       '#prefix' => '<div class="dashboard-link service-link">',
@@ -40,7 +40,7 @@ class Dashboard extends FormBase {
       '#suffix' => '</div>',
     ];
     $url = Url::fromRoute('view.organisations.page_1');
-    $link = Link::fromTextAndUrl('Manage organisation', $url)->toString();
+    $link = Link::fromTextAndUrl($this->t('Manage organisation'), $url)->toString();
     $form['manage_organization'] = [
       '#type' => 'markup',
       '#prefix' => '<div class="dashboard-link org-link">',
