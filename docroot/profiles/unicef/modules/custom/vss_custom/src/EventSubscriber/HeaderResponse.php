ee68bd5d84a441f4a5c36a13bfc0c87dbdd0115e
Talish Khan <talish.khan@srijan.net>
2022-02-09 13:48:12 +0530

    Added CSP fixes

--- docroot/profiles/unicef/modules/custom/vss_custom/src/EventSubscriber/HeaderResponse.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/EventSubscriber/HeaderResponse.php
@@ -25,7 +25,9 @@ class HeaderResponse implements EventSubscriberInterface {
     $response->headers->set("Access-Control-Allow-Origin", $event->getRequest()->getHost());
 
     $csp = "default-src 'self' blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.ampproject.org https://www.google-analytics.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://code.jquery.com https://cdn.jsdelivr.net https://www.youtube.com https://www.vimeo.com; img-src 'self' data: https://www.google-analytics.com https://h5p.org https://www.gstatic.com https://www.youtube.com https://www.vimeo.com  https://i.ytimg.com; style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com https://translate.googleapis.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com; font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com; frame-src 'self' https://www.youtube.com https://www.vimeo.com; connect-src 'self' https://www.google-analytics.com https://metrics.articulate.com; worker-src 'self' blob:;";
+    $connect_src = "connect-src https://api.opencagedata.com";
     $response->headers->set("Content-Security-Policy", $csp);
+    $response->headers->set("Content-Security-Policy", $connect_src);
   }
 
   /**
