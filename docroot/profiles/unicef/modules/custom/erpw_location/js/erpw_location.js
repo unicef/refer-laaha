08bbe27cea59e6779b6a52f0957f52d48c7878f0
Jay Mishra <69897982+jaysrijan@users.noreply.github.com>
2022-02-11 15:17:38 +0530

    NCF-231: eRPW - edit referral pathway template (#794)
    * [NCF-231]: eRPW - Edit Referral Pathway Template
    
    * NCF-231: eRPW- resolved composer lock

--- docroot/profiles/unicef/modules/custom/erpw_location/js/erpw_location.js
+++ docroot/profiles/unicef/modules/custom/erpw_location/js/erpw_location.js
@@ -7,6 +7,18 @@
                     window.location.href = drupalSettings.erpw_location.redirect_to_manage;
                 });
             });
+
+            // Adding class to hide the elements for location dropdown on forms.
+            $(".loc-dropdown").on('change', function() {
+              var level = $(this).attr("data-level");
+              var levelcount = drupalSettings.leveldetails.levelcount;
+              for (i = level; i <= levelcount; i++) {
+                $("#edit-level-" + i).addClass('hidden');
+                $("#edit-level-" + i).find('option:not(:first)').remove();
+              }
+              // Make last dropdown as multi select element.
+              $(".add_multiple").prop("multiple", 'true');
+          });
         }
     };
 }(jQuery, Drupal, drupalSettings));
