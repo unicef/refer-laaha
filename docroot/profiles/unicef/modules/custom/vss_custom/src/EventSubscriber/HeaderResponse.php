13389e0a7e7f1018807fbce60b125af14cf9100a
Talish Khan <talish.khan@srijan.net>
2022-02-11 13:16:05 +0530

    Added CSP fixes

--- docroot/profiles/unicef/modules/custom/vss_custom/src/EventSubscriber/HeaderResponse.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/EventSubscriber/HeaderResponse.php
@@ -23,12 +23,23 @@ class HeaderResponse implements EventSubscriberInterface {
     $response->headers->set("X-Content-Type-Options", "nosniff always");
     $response->headers->set("Strict-Transport-Security", "max-age=31536000; includeSubDomains always");
     $response->headers->set("Access-Control-Allow-Origin", $event->getRequest()->getHost());
-
-    $csp = "default-src 'self' blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://bam.nr-data.net/ https://www.googletagmanager.com https://js-agent.newrelic.com https://cdn.ampproject.org https://www.google-analytics.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://code.jquery.com https://cdn.jsdelivr.net https://www.youtube.com https://www.vimeo.com; img-src 'self' data: https://www.google-analytics.com https://h5p.org https://www.gstatic.com https://www.youtube.com https://www.vimeo.com  https://i.ytimg.com; style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com https://translate.googleapis.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com; font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com; frame-src 'self' https://www.youtube.com https://www.vimeo.com; connect-src 'self' https://www.google-analytics.com https://metrics.articulate.com; worker-src 'self' blob:;";
-    $connect_src = "connect-src https://api.opencagedata.com";
-    $frame_src = "frame-src 'self' https://player.vimeo.com https://iq.virtualsafespace.net https://ec.virtualsafespace.net https://virtualsafespace.net *.virtualsafespace.net";
+    $csp = "default-src 'self' blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval'
+      https://bam.nr-data.net https://cdn.ampproject.org https://www.google-analytics.com https://cdnjs.cloudflare.com
+      https://ajax.googleapis.com https://code.jquery.com https://cdn.jsdelivr.net https://www.youtube.com
+      https://www.googletagmanager.com https://js-agent.newrelic.com https://www.vimeo.com;
+      img-src 'self' data: https://www.google-analytics.com https://h5p.org
+      https://www.gstatic.com https://www.youtube.com https://www.vimeo.com  https://i.ytimg.com;
+      style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com *.virtualsafespace.net
+      https://translate.googleapis.com https://cdn.jsdelivr.net
+      https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com;
+      font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com *.virtualsafespace.net
+      https://www.youtube.com https://www.vimeo.com https://i.ytimg.com;
+      frame-src 'self' https://www.youtube.com https://www.vimeo.com https://player.vimeo.com https://iq.virtualsafespace.net
+      https://ec.virtualsafespace.net https://virtualsafespace.net *.virtualsafespace.net;
+      connect-src 'self' https://www.google-analytics.com https://metrics.articulate.com *.virtualsafespace.net
+      https://api.opencagedata.com;
+      worker-src 'self' blob:;";
     $response->headers->set("Content-Security-Policy", $csp);
-    $response->headers->set("Content-Security-Policy", $frame_src);
   }
 
   /**
