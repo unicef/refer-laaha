ddc7dbf16dff47fa441d52c9bffb3d08ca7489ca
Jay Mishra <69897982+jaysrijan@users.noreply.github.com>
2022-02-16 18:40:19 +0530

    NCF-1232/1234: eRPW: Fixed Add/Edit Referral path way issue (#875)

--- docroot/profiles/unicef/modules/custom/erpw_location/modules/erpw_pathway/js/erpw_pathway.js
+++ docroot/profiles/unicef/modules/custom/erpw_location/modules/erpw_pathway/js/erpw_pathway.js
@@ -2,16 +2,16 @@
   Drupal.behaviors.location = {
     attach: function (context, settings) {
       $(".loc-dropdown").on('change', function() {
-        var level = $(this).attr("data-level");
-        var levelcount = drupalSettings.leveldetails.levelcount;
-        for (var i = level; i <= levelcount; i++) {
+        let level = $(this).attr("data-level");
+        let levelcount = drupalSettings.leveldetails.levelcount;
+        for (let i = level; i <= levelcount; i++) {
           $("#location-level-" + i).addClass('hidden');
           $("#location-level-" + i).find('option:not(:first)').remove();
         }
         // Make last dropdown as multi select element.
         $(".add_multiple").prop("multiple", 'true');
       });
-      //Referral Pathway for add another section
+      // Adding icon class for Referral Pathway for add another button.
       $('.node-referral-path-way-form .field-add-more-submit, .node-referral-path-way-edit-form  .field-add-more-submit').parents('.clearfix').addClass('plus-icon button-with-icon');
     }
   };
