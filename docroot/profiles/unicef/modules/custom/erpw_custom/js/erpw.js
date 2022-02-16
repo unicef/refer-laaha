f1e3b948da9bfd79e53720840e8840b9808b3449
preetichawla29 <preeti.chawla8286@gmail.com>
2022-02-16 16:50:39 +0530

    NCF-1059: select country location mgt (#870)
    * NCF-1059: select country location mgt
    
    * NCF-1059: select country location mgt
    
    Co-authored-by: preetichawla29 <preetichawla8286@gmail.com>

--- docroot/profiles/unicef/modules/custom/erpw_custom/js/erpw.js
+++ docroot/profiles/unicef/modules/custom/erpw_custom/js/erpw.js
@@ -55,6 +55,11 @@
             
             //Add select 2 for multiselect
             $('.form-select[multiple=multiple]').select2();
+            
+            //Home page country field
+            if ($('.user-location').text().length > 0) {
+                $('.user-location').addClass('location-theme');
+            }
         }
     };
 }(jQuery, Drupal, drupalSettings));
