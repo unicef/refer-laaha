0218fd45f762778a79f722a5e2fce26b8dd4ee73
Talish Khan <talish.khan@srijan.net>
2022-02-18 18:09:33 +0530

    FIxing CSP issues

--- docroot/profiles/unicef/modules/custom/vss_custom/src/EventSubscriber/HeaderResponse.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/EventSubscriber/HeaderResponse.php
@@ -20,18 +20,18 @@ class HeaderResponse implements EventSubscriberInterface {
     $response->headers->remove('Generator');
 
     $response->headers->set("X-XSS-Protection", "1; mode=block");
-    $response->headers->set("X-Frame-Options", "ALLOW-FROM https://dev.virtualsafespace.net https://dev.iq.virtualsafespace.net https://dev.ec.virtualsafespace.net " . $event->getRequest()->getHost());
+    $response->headers->set("X-Frame-Options", "ALLOW-FROM dev.virtualsafespace.net dev.iq.virtualsafespace.net dev.ec.virtualsafespace.net " . $event->getRequest()->getHost());
     $response->headers->set("X-Content-Type-Options", "nosniff always");
     $response->headers->set("Strict-Transport-Security", "max-age=31536000; includeSubDomains always");
     $response->headers->set("Access-Control-Allow-Origin", $event->getRequest()->getHost());
 
     $csp = "upgrade-insecure-requests; default-src 'self' blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://bam.nr-data.net https://cdn.ampproject.org https://www.google-analytics.com https://cdnjs.cloudflare.com https://ajax.googleapis.com https://code.jquery.com https://cdn.jsdelivr.net https://www.youtube.com https://www.googletagmanager.com https://js-agent.newrelic.com https://www.vimeo.com;";
 
-    $csp .= " img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com https://h5p.org https://www.gstatic.com https://www.youtube.com https://www.vimeo.com  https://i.ytimg.com;";
+    $csp .= " img-src 'self' data: http://stage.virtualsafespace.net http://stage.iq.virtualsafespace.net http://stage.ec.virtualsafespace.net http://dev.virtualsafespace.net http://dev.iq.virtualsafespace.net http://dev.ec.virtualsafespace.net  https://iq.virtualsafespace.net https://ec.virtualsafespace.net https://virtualsafespace.net https://www.googletagmanager.com https://www.google-analytics.com https://h5p.org https://www.gstatic.com https://www.youtube.com https://www.vimeo.com  https://i.ytimg.com;";
 
-    $csp .= " style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com *.virtualsafespace.net https://translate.googleapis.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com;";
+    $csp .= " style-src 'self' 'unsafe-inline' 'unsafe-eval' http://stage.virtualsafespace.net http://stage.iq.virtualsafespace.net http://stage.ec.virtualsafespace.net http://dev.virtualsafespace.net http://dev.iq.virtualsafespace.net http://dev.ec.virtualsafespace.net  https://iq.virtualsafespace.net https://ec.virtualsafespace.net https://virtualsafespace.net https://fonts.googleapis.com *.virtualsafespac e.net https://translate.googleapis.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://www.vimeo.com https://i.ytimg.com;";
 
-    $csp .= " font-src 'self' data: https://themes.googleusercontent.com https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com *.virtualsafespace.net https://www.youtube.com https://www.vimeo.com https://i.ytimg.com;";
+    $csp .= " font-src 'self' data: http://stage.virtualsafespace.net http://stage.iq.virtualsafespace.net http://stage.ec.virtualsafespace.net http://dev.virtualsafespace.net http://dev.iq.virtualsafespace.net http://dev.ec.virtualsafespace.net  https://iq.virtualsafespace.net https://ec.virtualsafespace.net https://virtualsafespace.net https://themes.googleusercontent.com https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com *.virtualsafespace.net https://www.youtube.com https://www.vimeo.com https://i.ytimg.com;";
 
     $csp .= " frame-src 'self' http://stage.virtualsafespace.net http://stage.iq.virtualsafespace.net http://stage.ec.virtualsafespace.net http://dev.virtualsafespace.net http://dev.iq.virtualsafespace.net http://dev.ec.virtualsafespace.net https://www.youtube.com https://www.vimeo.com https://player.vimeo.com https://iq.virtualsafespace.net https://ec.virtualsafespace.net https://virtualsafespace.net *.virtualsafespace.net;";
 
