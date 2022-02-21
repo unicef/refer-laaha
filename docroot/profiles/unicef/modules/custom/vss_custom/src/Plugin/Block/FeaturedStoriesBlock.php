2f861eea0e7f49acf774285f75a1b7bbf8e24a99
srikant-dubey <srikant-dubey@users.noreply.github.com>
2022-02-21 16:11:35 +0530

    Dev Stage Sync  (#918)
    * NCF-932: label display block.
    
    * NCF-000: fixes UI
    
    * Enable/disable module
    
    * NCF-1224: cleanup menu and signup.
    
    * NCF-1224: cleanup menu and signup. (#902)
    
    * NCF-932: label display block.
    
    * NCF-000: fixes UI
    
    * NCF-1224: cleanup menu and signup.
    
    Co-authored-by: preetichawla29 <preetichawla8286@gmail.com>
    
    * Added amp for faq page
    
    * NCF-1039 : mapping user roles (#903)
    
    * NCF-1224: cleanup menu and signup.
    
    * Fixing CSP issues
    
    * FIxing CSP issues
    
    * NCF-231: eRPW-Fixed- add more section and its drag and drop (#910)
    
    * Reverting scorm patch
    
    * Remove seckit settings
    
    * module changes
    
    * module changes
    
    * NCF-1077 : CA role bug on RPW. (#914)
    
    * module changes
    
    * module changes
    
    * module changes
    
    Co-authored-by: preetichawla29 <preetichawla8286@gmail.com>
    Co-authored-by: Adil Siddiqui <adil.siddiqui@srijan.net>
    Co-authored-by: preetichawla29 <preeti.chawla8286@gmail.com>
    Co-authored-by: jaheer3 <jahirul.islam@srijan.in>
    Co-authored-by: Sagar Patel <98081605+sagar7567@users.noreply.github.com>
    Co-authored-by: Talish Khan <talish.khan@srijan.net>
    Co-authored-by: Talish Khan <66788898+talishkhan26@users.noreply.github.com>
    Co-authored-by: Jay Mishra <69897982+jaysrijan@users.noreply.github.com>

--- docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/FeaturedStoriesBlock.php
+++ docroot/profiles/unicef/modules/custom/vss_custom/src/Plugin/Block/FeaturedStoriesBlock.php
@@ -72,43 +72,46 @@ class FeaturedStoriesBlock extends BlockBase implements ContainerFactoryPluginIn
       if ($count > 1) {
         $content[$k]['title'] = $v->title;
         $thumbnail = $v->field_thumbnail_image_target_id;
-        $node = \Drupal::entityTypeManager()->getStorage('node')->load($v->nft_entity_id);
-        if (!$node->get('field_read_time')->isEmpty()) {
-          $read_time = $node->field_read_time->getValue()['0']['value'];
-          $content[$k]['read_time'] = $read_time;
-        }
+        $node = $this->entityTypeManager->getStorage('node')->load($v->nft_entity_id);
+        if ($node->hasTranslation($langcode) || $node->get('langcode')->value == $langcode) {
+          $node = $node->getTranslation($langcode);
+          if (!$node->get('field_read_time')->isEmpty()) {
+            $read_time = $node->field_read_time->getValue()['0']['value'];
+            $content[$k]['read_time'] = $read_time;
+          }
 
-        $paragraph_video_time = NULL;
-        if ($v->type == 'video') {
-          if (!$node->get('field_content')->isEmpty()) {
-            $paragraph_id = $node->get('field_content')->getValue();
-            foreach ($paragraph_id as $content_id) {
-              $paragraph_obj = Paragraph::load($content_id['target_id']);
-              $paragraph_type = $paragraph_obj->get('type')->getValue()['0']['target_id'];
-              if ($paragraph_type == "video") {
-                if (!$paragraph_obj->get('field_video_time')->isEmpty()) {
-                  $paragraph_video_time = $paragraph_obj->get('field_video_time')->getValue()['0']['value'];
-                  break;
+          $paragraph_video_time = NULL;
+          if ($v->type == 'video') {
+            if (!$node->get('field_content')->isEmpty()) {
+              $paragraph_id = $node->get('field_content')->getValue();
+              foreach ($paragraph_id as $content_id) {
+                $paragraph_obj = Paragraph::load($content_id['target_id']);
+                $paragraph_type = $paragraph_obj->get('type')->getValue()['0']['target_id'];
+                if ($paragraph_type == "video") {
+                  if (!$paragraph_obj->get('field_video_time')->isEmpty()) {
+                    $paragraph_video_time = $paragraph_obj->get('field_video_time')->getValue()['0']['value'];
+                    break;
+                  }
                 }
               }
             }
           }
-        }
 
-        $paragraph_podcast_time = NULL;
-        if ($v->type == 'podcast') {
-          if (!$node->get('field_content')->isEmpty()) {
-            $paragraph_podcast = $node->get('field_content')->getValue();
-            foreach ($paragraph_podcast as $content_pod_id) {
-              $paragraph_pod_obj = Paragraph::load($content_pod_id['target_id']);
-              $paragraph_pod_type = $paragraph_pod_obj->get('type')->getValue()['0']['target_id'];
-              if ($paragraph_pod_type == "podcast_audio") {
-                if (!$paragraph_pod_obj->get('field_podcast_time')->isEmpty()) {
-                  $paragraph_podcast_time = $paragraph_pod_obj->get('field_podcast_time')->getValue()['0']['value'];
-                  break;
+          $paragraph_podcast_time = NULL;
+          if ($v->type == 'podcast') {
+            if (!$node->get('field_content')->isEmpty()) {
+              $paragraph_podcast = $node->get('field_content')->getValue();
+              foreach ($paragraph_podcast as $content_pod_id) {
+                $paragraph_pod_obj = Paragraph::load($content_pod_id['target_id']);
+                $paragraph_pod_type = $paragraph_pod_obj->get('type')->getValue()['0']['target_id'];
+                if ($paragraph_pod_type == "podcast_audio") {
+                  if (!$paragraph_pod_obj->get('field_podcast_time')->isEmpty()) {
+                    $paragraph_podcast_time = $paragraph_pod_obj->get('field_podcast_time')->getValue()['0']['value'];
+                    break;
+                  }
                 }
-              }
 
+              }
             }
           }
         }
@@ -122,12 +125,13 @@ class FeaturedStoriesBlock extends BlockBase implements ContainerFactoryPluginIn
         $content[$k]['type'] = $v->type;
         $content[$k]['video_time'] = $paragraph_video_time;
         $content[$k]['podcast_time'] = $paragraph_podcast_time;
+
+        $this->pageCacheKillSwitch->trigger();
+        $build['#theme'] = 'featured_stories_block';
+        $build['#content'] = $content;
+        $build['#lang_code'] = $langcode;
       }
     }
-    $this->pageCacheKillSwitch->trigger();
-    $build['#theme'] = 'featured_stories_block';
-    $build['#content'] = $content;
-    $build['#lang_code'] = $langcode;
 
     return $build;
   }
@@ -151,10 +155,12 @@ class FeaturedStoriesBlock extends BlockBase implements ContainerFactoryPluginIn
     $query->join('node__field_thumbnail_image', 'tmb', 'nft.entity_id = tmb.entity_id');
     $query->join('node__field_sub_category', 'nscat', 'nscat.entity_id = nft.entity_id');
     $query->join('taxonomy_term__parent', 'tp', 'tp.entity_id = nscat.field_sub_category_target_id AND tp.parent_target_id = t.tid');
-    $query->join('node_field_data', 'n', 'nft.entity_id = n.nid');
-    // $query->join('node__field_read_time', 'frt', 'nft.entity_id = n.nid');
+    $query->join('node_field_data', 'n', "nft.entity_id = n.nid AND n.langcode = $langcode");
+    $query->join('node__field_domain_access', 'nd', 'nft.entity_id = nd.entity_id');
+    $query->condition('nd.field_domain_access_target_id', $domain);
     $query->condition('t.vid', 'categories');
-    $query->condition('n.langcode', $langcode);
+    $query->condition('nft.langcode', $langcode);
+    $query->condition('t.langcode', $langcode);
     $query->condition('t.tid', $term_id);
     $query->condition('dm.field_domain_target_id', $domain);
     $query->condition('sc.field_sub_category_value', 1, '!=');
