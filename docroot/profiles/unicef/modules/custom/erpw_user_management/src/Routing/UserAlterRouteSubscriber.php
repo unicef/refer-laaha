43b0a3f3dc24f596c46ee88b984f6edd14742c83
sager.patel <sagar.patel@srijan.net>
2022-03-08 10:19:22 +0530

    NCF-1318 fixed location filter issue with permission.

---
+++ docroot/profiles/unicef/modules/custom/erpw_user_management/src/Routing/UserAlterRouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\erpw_user_management\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class UserAlterRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    $permission = erpw_user_management_user_form_access();
+    if ($permission && $route = $collection->get('entity.user.edit_form')) {
+      $route->setOption('_admin_route', FALSE);
+    }
+  }
+
+}
