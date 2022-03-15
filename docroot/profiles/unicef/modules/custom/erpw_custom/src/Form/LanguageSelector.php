afd57d3078f7b686daef2dfe588c8120213d40f2
srikant-dubey <srikant-dubey@users.noreply.github.com>
2022-03-15 17:25:28 +0530

    Assign Location from domain (#1134)

--- docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/LanguageSelector.php
+++ docroot/profiles/unicef/modules/custom/erpw_custom/src/Form/LanguageSelector.php
@@ -190,7 +190,6 @@ class LanguageSelector extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $value = $form_state->getValues();
     if (!empty($value)) {
-      $default_country_tid = FALSE;
       $domain = $this->entityManager->getStorage('domain')->load($form_state->getValue('country'));
       $domain_lang = $form_state->getValue('language_selector');
       $domain_path = $domain->get('path');
@@ -200,31 +199,14 @@ class LanguageSelector extends FormBase {
       $domain_site = '.' . $domain_slice[0] . '.' . $domain_slice[1];
       setcookie('userLanguageSelection', 'TRUE', strtotime('+1 year'), '/', $domain_site, FALSE);
       setcookie('userLanguage', $value['language_selector'], strtotime('+1 year'), '/', $domain_site, FALSE);
+      $config = $this->configfactory->getEditable('domain.location.' . $domain->get('id'));
+      if (!$default_location = $config->get('location')) {
+        $default_location = $this->locationService->getDefaultLocation();
+      }
       // Storing the value into cookie and temp storage.
       if ($this->currentUser->isAuthenticated()) {
         $user = $this->entityManager->getStorage('user')->load($this->currentUser->id());
-        $default_location = $this->locationService->getUserDefaultLocation($user);
-      }
-      elseif ($this->currentUser->isAnonymous()) {
-        // Get the default location for current domain.
-        foreach (ERPW_COUNTRY_DOMAIN_MAPPING as $country) {
-          if (strpos($domain_path, $country['code']) !== FALSE) {
-            $query = $this->connection->select('taxonomy_term__field_country_code', 'cc');
-            $query->fields('cc', ['entity_id', 'field_country_code_value']);
-            $query->condition('cc.field_country_code_value', $country['code']);
-            $query->range(0, 1);
-            $result = $query->execute()->fetchAll();
-            // If country code exists.
-            if (!empty($result)) {
-              $default_location = $result[0]->entity_id;
-              $default_country_tid = TRUE;
-              break;
-            }
-          }
-        }
-        if (!$default_country_tid) {
-          $default_location = $this->locationService->getDefaultLocation();
-        }
+        $default_location = ($this->locationService->getUserDefaultLocation($user)) ?? $default_location;
       }
       if (empty($this->locationCookie->getCookieValue())) {
         $this->locationCookie->setCookieValue(base64_encode('country_tid_' . time()));
