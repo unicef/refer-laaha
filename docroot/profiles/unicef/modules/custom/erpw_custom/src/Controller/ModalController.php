c7b8d2f7b736a8dc74543e20ce5875872608b315
Sagar Patel <98081605+sagar7567@users.noreply.github.com>
2022-03-15 09:22:48 +0530

    NCF-745 adding custom exposed filter in view service listing (#989) (#1120)
    Co-authored-by: Roshni-Kodiganti <“kodiganti.roshni@srijan.netgit config --global user.email “kodiganti.roshni@srijan.net>
    
    User approval block

--- docroot/profiles/unicef/modules/custom/erpw_custom/src/Controller/ModalController.php
+++ docroot/profiles/unicef/modules/custom/erpw_custom/src/Controller/ModalController.php
@@ -2,12 +2,16 @@
 
 namespace Drupal\erpw_custom\Controller;
 
-use Drupal\node\Entity\Node;
 use Drupal\Core\Form\FormBuilder;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Url;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * ModalFormController class.
@@ -22,13 +26,23 @@ class ModalController extends ControllerBase {
   protected $formBuilder;
 
   /**
+   * A entityManager instance.
+   *
+   * @var Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityManager;
+
+  /**
    * The ModalFormController constructor.
    *
    * @param \Drupal\Core\Form\FormBuilder $form_builder
    *   The form builder.
+   * @param Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   EntityManager object.
    */
-  public function __construct(FormBuilder $form_builder) {
+  public function __construct(FormBuilder $form_builder, EntityTypeManagerInterface $entity_type_manager) {
     $this->formBuilder = $form_builder;
+    $this->entityManager = $entity_type_manager;
   }
 
   /**
@@ -41,7 +55,8 @@ class ModalController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('form_builder')
+      $container->get('form_builder'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -50,7 +65,7 @@ class ModalController extends ControllerBase {
    */
   public function deleteServiceTypeNode($id) {
     $response = new AjaxResponse();
-    $node = Node::load($id);
+    $node = $this->entityManager->getStorage('node')->load($id);
     if (!empty($node)) {
       $node->delete();
     }
@@ -70,6 +85,18 @@ class ModalController extends ControllerBase {
     return [
       '#markup' => '',
     ];
+  }
+
+  /**
+   * Active user.
+   */
+  public function activeUser(AccountInterface $user, Request $request) {
+    $user->activate();
+    $user->save();
+    $url = Url::fromRoute('view.user_lists.page_2')->toString();
+    $response = new RedirectResponse($url);
+    $response->send();
+    return $response;
   }
 
 }
