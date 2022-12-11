62985953e22ad24792659408ddd1c45ac451f480
Benjamin Melançon <ben@agaric.coop>
2022-12-10 22:44:20 -0500

    Remove irrelevant references to shield settings

--- docroot/sites/default/settings.prod.php
+++ docroot/sites/default/settings.prod.php
@@ -11,8 +11,3 @@ $config['config_split.config_split.local']['status'] = FALSE;
 $config['environment_indicator.indicator']['bg_color'] = '#f44336';
 $config['environment_indicator.indicator']['fg_color'] = '#FFFFFF';
 $config['environment_indicator.indicator']['name'] = 'VSS Production';
-
-// Disabling shield module on prod.
-$config['shield.settings']['shield_enable'] = 0;
-$config['sheild.settings']['credentials.shield.user'] = '';
-$config['sheild.settings']['credentials.shield.pass'] = '';
