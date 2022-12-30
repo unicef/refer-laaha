eaf30ea2799e44b3994a60b08051e5feeebf4cc4
Benjamin Melançon <ben@agaric.coop>
2022-12-30 18:16:08 -0500

    Add country subdomain to URL we redirect to #1324

--- docroot/modules/custom/erpw_location/src/Form/UserLocationForm.php
+++ docroot/modules/custom/erpw_location/src/Form/UserLocationForm.php
@@ -171,9 +171,22 @@ class UserLocationForm extends LocationListForm {
       $domain_site = '.' . $domain_slice[0] . '.' . $domain_slice[1];
 
       setcookie('location_tid', $location_value, strtotime('+1 year'), '/', $domain_site, FALSE);
+
       $url = Url::fromRoute('view.referral_pathway_on_homepage.page_1', [], ['query' => ['location' => $location_value]]);
+      // First level is the country taxonomy term; if we have it we want to
+      // modify the redirect to include the matching subdomain.
+      if (!empty($levels['level_1'])) {
+        $tid = $levels['level_1'];
+        // Ridiculous Drupal has no apparent way to change the domain/subdomain in its URL
+        $url->setAbsolute();
+        $url_string = $url->toString();
+        $url_array = parse_url($url_string);
+        $url_host = erpw_location_country_code_from_tid($tid) . '.' . $url_array['host'];
+        $url_array['host'] = $url_host;
+        $url_string = erpw_location_unparse_url($url_array);
+        $url = Url::fromUri($url_string);
+      }
       $form['location_level']['button'] = $form_state->setRedirectUrl($url);
     }
   }
-
 }
