185eadd8ffaf60976b381f12cf36f3eb8866c677
nishtha-pradhan5 <nishtha.pradhan@qed42.com>
2023-06-07 14:07:53 +0530

    Add cache context for Homapage node + some other fixes related to webform.

--- docroot/modules/custom/erpw_webform/src/Plugin/WebformHandler/DuplicateSubmissionWebformHandler.php
+++ docroot/modules/custom/erpw_webform/src/Plugin/WebformHandler/DuplicateSubmissionWebformHandler.php
@@ -26,33 +26,43 @@ class DuplicateSubmissionWebformHandler extends WebformHandlerBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
     $location_values = $webform_submission->getElementData('location');
-    $level_4_value = $location_values['level_4'] ?? '';
+    $country_id = $location_values['location_options'];
+    if ($country_id == '1' || $country_id == '16') {
+      $last_level = 'level_3';     
+    }
+    else {
+      $last_level = 'level_4';
+    }
+    $last_level_value = $location_values[$last_level] ?? '';
     $organisation_value = $webform_submission->getElementData('organisation');
 
     // Check if there is an existing submission with the same location
     // for level_4, and organisation values.
-    $query = \Drupal::database()->select('webform_submission_data', 'wsd');
-    $query->fields('wsd', ['sid']);
-    $query->condition('wsd.webform_id', $webform_submission->getWebform()->id());
-    $query->condition('wsd.property', 'level_4');
-    $query->condition('wsd.value', $level_4_value);
+    $existing_submission_ids = [];
+    if ($last_level_value != "") {
+      $query = \Drupal::database()->select('webform_submission_data', 'wsd');
+      $query->fields('wsd', ['sid']);
+      $query->condition('wsd.webform_id', $webform_submission->getWebform()->id());
+      $query->condition('wsd.property', $last_level);
+      $query->condition('wsd.value', $last_level_value);
 
-    $query->join('webform_submission_data', 'wsd2', 'wsd.sid = wsd2.sid');
-    $query->condition('wsd2.name', 'organisation');
-    $query->condition('wsd2.value', $organisation_value);
+      $query->join('webform_submission_data', 'wsd2', 'wsd.sid = wsd2.sid');
+      $query->condition('wsd2.name', 'organisation');
+      $query->condition('wsd2.value', $organisation_value);
 
-    $query->join('webform_submission_data', 'wsd3', 'wsd.sid = wsd3.sid');
-    $query->condition('wsd3.property', 'workflow_state');
-    $query->condition('wsd3.value', 'reject', '!=');
+      $query->join('webform_submission_data', 'wsd3', 'wsd.sid = wsd3.sid');
+      $query->condition('wsd3.property', 'workflow_state');
+      $query->condition('wsd3.value', 'reject', '!=');
 
-    $existing_submission_ids = $query->execute()->fetchAll();
+      $existing_submission_ids = $query->execute()->fetchAll();
 
-    // Exclude current submission id from the list.
-    $existing_sids = array_map(function ($submission) {
-      return $submission->sid;
-    }, $existing_submission_ids);
+      // Exclude current submission id from the list.
+      $existing_sids = array_map(function ($submission) {
+        return $submission->sid;
+      }, $existing_submission_ids);
 
-    $existing_submission_ids = array_diff($existing_sids, [$webform_submission->id()]);
+      $existing_submission_ids = array_diff($existing_sids, [$webform_submission->id()]);
+    }
 
     // If an existing submission is found,
     // display an error message and stop the form validation.
