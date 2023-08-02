d83d413d2f0869c9b69c313f82c0a8454df727f9
Hari Venu <hari.venu@qed42.com>
2023-08-02 18:36:21 +0530

    zoho - 403 - SP Staff action link definition.

--- docroot/modules/custom/erpw_custom/src/Controller/UserWorkflowController.php
+++ docroot/modules/custom/erpw_custom/src/Controller/UserWorkflowController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\Core\Url;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\user\UserInterface;
 
 /**
  * ModalFormController class.
@@ -48,7 +49,51 @@ class UserWorkflowController extends ControllerBase {
   /**
    * Accept User.
    */
-  public function acceptUser(AccountInterface $user, Request $request) {
+  public function acceptUser(UserInterface $user, Request $request) {
+    // Get current user role.
+    $roles = \Drupal::currentUser()->getRoles();
+    // For SPFP.
+    if (in_array('service_provider_focal_point', $roles)) {
+      // Make sure this is the right user take action.
+      if ($user->hasRole('service_provider_staff')) {
+        // Update the user with new transition.
+        $user->set('field_transitions', 'spfp-accept');
+        $user->save();
+
+        // Update user workflow history entity.
+        $current_time = \Drupal::time()->getCurrentTime('d');    
+        $euwh = $this->entityTypeManager->getStorage('user_workflow_history_entity')->create([
+          'name' => \Drupal::service('date.formatter')->format($current_time, 'custom', 'd/m/Y H:i:s'),
+          'status' => 1,
+          'field_user' => $user->id(),
+          'field_workflow_status_before' => 'self-register-sp-staff',
+          'field_workflow_status_after' => 'spfp-accept',
+        ]);
+        $euwh->save();
+      }
+    }
+
+    // For GBV Coordination.
+    if (in_array('interagency_gbv_coordinator', $roles) || in_array('country_admin', $roles)) {
+      // Make sure this is the right user take action.
+      if ($user->hasRole('service_provider_staff')) {
+        // Update the user with new transition.
+        $user->activate();
+        $user->set('field_transitions', 'gbv-coordination-accept');
+        $user->save();
+
+        // Update user workflow history entity.
+        $current_time = \Drupal::time()->getCurrentTime('d');    
+        $euwh = $this->entityTypeManager->getStorage('user_workflow_history_entity')->create([
+          'name' => \Drupal::service('date.formatter')->format($current_time, 'custom', 'd/m/Y H:i:s'),
+          'status' => 1,
+          'field_user' => $user->id(),
+          'field_workflow_status_before' => 'spfp-accept',
+          'field_workflow_status_after' => 'gbv-coordination-accept',
+        ]);
+        $euwh->save();
+      }
+    }
 
     $url = Url::fromRoute('view.user_lists.page_2')->toString();
     $response = new RedirectResponse($url);
@@ -59,7 +104,52 @@ class UserWorkflowController extends ControllerBase {
   /**
    * Reject User.
    */
-  public function rejectUser(AccountInterface $user, Request $request) {
+  public function rejectUser(UserInterface $user, Request $request) {
+
+    // Get current user role.
+    $roles = \Drupal::currentUser()->getRoles();
+    // For SPFP.
+    if (in_array('service_provider_focal_point', $roles)) {
+      // Make sure this is the right user take action.
+      if ($user->hasRole('service_provider_staff')) {
+        // Update the user with new transition.
+        $user->set('field_transitions', 'spfp-reject');
+        $user->save();
+
+        // Update user workflow history entity.
+        $current_time = \Drupal::time()->getCurrentTime('d');    
+        $euwh = $this->entityTypeManager->getStorage('user_workflow_history_entity')->create([
+          'name' => \Drupal::service('date.formatter')->format($current_time, 'custom', 'd/m/Y H:i:s'),
+          'status' => 1,
+          'field_user' => $user->id(),
+          'field_workflow_status_before' => 'self-register-sp-staff',
+          'field_workflow_status_after' => 'spfp-reject',
+        ]);
+        $euwh->save();
+      }
+    }
+
+    // For GBV Coordination.
+    if (in_array('interagency_gbv_coordinator', $roles) || in_array('country_admin', $roles)) {
+      // Make sure this is the right user take action.
+      if ($user->hasRole('service_provider_staff')) {
+        // Update the user with new transition.
+        $user->activate();
+        $user->set('field_transitions', 'gbv-coordination-reject');
+        $user->save();
+
+        // Update user workflow history entity.
+        $current_time = \Drupal::time()->getCurrentTime('d');    
+        $euwh = $this->entityTypeManager->getStorage('user_workflow_history_entity')->create([
+          'name' => \Drupal::service('date.formatter')->format($current_time, 'custom', 'd/m/Y H:i:s'),
+          'status' => 1,
+          'field_user' => $user->id(),
+          'field_workflow_status_before' => 'spfp-accept',
+          'field_workflow_status_after' => 'gbv-coordination-reject',
+        ]);
+        $euwh->save();
+      }
+    }
 
     $url = Url::fromRoute('view.user_lists.page_2')->toString();
     $response = new RedirectResponse($url);
