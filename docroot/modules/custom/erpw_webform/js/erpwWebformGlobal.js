c6282169c778f712f0a2bb29db299136ff2ef0d5
nishtha-pradhan5 <nishtha.pradhan@qed42.com>
2023-08-03 18:35:19 +0530

    Change styling of Delete transition buttons to less prominent.

--- docroot/modules/custom/erpw_webform/js/erpwWebformGlobal.js
+++ docroot/modules/custom/erpw_webform/js/erpwWebformGlobal.js
@@ -15,6 +15,13 @@
           "true"
         );
         $('.erpw-workflow-cta-button:contains("Save as Draft")').addClass('draft-transition');
+
+        // Change the style of Delete Buttons of the Service Workflow
+        const deleteTransitions = ['Submit for Deletion to Focal Point', 'Submit for Deletion to GBV Coordination', 'Delete Service'];
+        deleteTransitions.forEach(transition => {
+          $('.erpw-workflow-cta-button:contains(' + transition + ')').addClass('delete-transition');
+        });
+
       });
     },
   };
