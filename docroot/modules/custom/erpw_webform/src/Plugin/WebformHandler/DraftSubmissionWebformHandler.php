f4dd7b6c792d4098c4a22ff8c41c8152f5006b18
nishtha-pradhan5 <nishtha.pradhan@qed42.com>
2023-08-04 17:41:53 +0530

    Change Redirection for Service Forms

--- docroot/modules/custom/erpw_webform/src/Plugin/WebformHandler/DraftSubmissionWebformHandler.php
+++ docroot/modules/custom/erpw_webform/src/Plugin/WebformHandler/DraftSubmissionWebformHandler.php
@@ -53,27 +53,32 @@ class DraftSubmissionWebformHandler extends WebformHandlerBase {
   public function confirmForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
 
     $workflow_selected = $webform_submission->getElementData('erpw_workflow');
-    $alias = '/service-providers';
-    $path = \Drupal::service('path_alias.manager')->getPathByAlias($alias);
-    $url = Url::fromUri('internal:' . $path);
-    $parameters = $url->getRouteParameters();
-    if (isset($parameters['node'])) {
+    $draft_alias = '/service-providers';
+    $manage_service_alias = '/manage-services';
+    $draft_path = \Drupal::service('path_alias.manager')->getPathByAlias($draft_alias);
+    $manage_service_path = \Drupal::service('path_alias.manager')->getPathByAlias($manage_service_alias);
+    $draft_url = Url::fromUri('internal:' . $draft_path);
+    $manage_service_url = Url::fromUri('internal:' . $manage_service_path);
+    $parameters = $draft_url->getRouteParameters();
+    $parameters2 = $manage_service_url->getRouteParameters();
+    if (isset($parameters['node']) || isset($parameters2['node'])) {
       $node = Node::load($parameters['node']);
-      if ($node) {
-        // Redirect the user to their saved Drafts if their transition was Save as Draft.
-        if ($workflow_selected['transition'] == "save_as_draft") {
-          $query = [
-            'service_type' => 'All',
-            'webform_submission_workflow_filter' => 'draft',
-          ];
-          $this->messenger->addMessage($this->t('Draft Saved Successfully!'));
-          $form_state->setRedirect('entity.node.canonical', ['node' => $node->id()], ['query' => $query]);
-        }
-        // For all other transitions, redirect them to Review page.
-        else {
-          $this->messenger->addMessage($this->t('Changes Saved Successfully!'));
-          $form_state->setRedirect('entity.node.canonical', ['node' => $node->id()]);
-        }
+      $manage_service_node = Node::load($parameters2['node']);
+
+      // Redirect the user to their saved Drafts if their transition was Save as Draft.
+      if ($workflow_selected['transition'] == "save_as_draft") {
+        $query = [
+          'service_type' => 'All',
+          'webform_submission_workflow_filter' => 'draft',
+        ];
+        $this->messenger->addMessage($this->t('Draft Saved Successfully!'));
+        $form_state->setRedirect('entity.node.canonical', ['node' => $node->id()], ['query' => $query]);
+      }
+
+      // For all other transitions, redirect them to Manage Services page.
+      else {
+        $this->messenger->addMessage($this->t('Changes Saved Successfully!'));
+        $form_state->setRedirect('entity.node.canonical', ['node' => $manage_service_node->id()]);
       }
     }
   }
