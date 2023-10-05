c264fef4378abae55da9b763d8a70b9280f38e40
Anjali Rathod <anjali.rathod@qed42.com>
2023-10-05 17:05:26 +0530

    Added the logic to remove the row on review and submit from indexeddb.

--- docroot/modules/custom/erpw_webform/js/erpwOfflineServiceFormEdit.js
+++ docroot/modules/custom/erpw_webform/js/erpwOfflineServiceFormEdit.js
@@ -10,16 +10,15 @@
       // Set the flag to indicate that the code has been executed
       alreadyExecuted = true;
 
+      var currentUserId = drupalSettings.user.uid;
+      localforageUserServiceChanges = localforage.createInstance({
+        driver: localforage.INDEXEDDB,
+        name: "userServiceChanges".concat(currentUserId),
+        version: 1.0,
+        storeName: "userServiceChanges".concat(currentUserId),
+      });
       $(document).ready(function () {
-        var currentUserId = drupalSettings.user.uid;
         if (currentUserId != 0) {
-          // Inside your fetchDataAndStore function:
-          localforageUserServiceChanges = localforage.createInstance({
-            driver: localforage.INDEXEDDB, // You can choose the storage driver you prefer
-            name: "userServiceChanges".concat(currentUserId),
-            version: 1.0,
-            storeName: "userServiceChanges".concat(currentUserId),
-          });
           // Check if localforageUserServiceChanges has any key-value pairs
           localforageUserServiceChanges
             .length()
@@ -29,6 +28,16 @@
                   .iterate(function (valueData, key, iterationNumber) {
                     var url = window.location.href;
                     if (key == url.match(/\/(\d+)\/edit$/)[1]) {
+                      // Select all elements with the class erpw-workflow-cta-button.
+                      var buttons = document.querySelectorAll(
+                        ".erpw-workflow-cta-button"
+                      );
+
+                      // Loop through the selected buttons and add the click event listener to each.
+                      buttons.forEach(function (button) {
+                        button.addEventListener("click", buttonClickHandler);
+                      });
+
                       for (var fieldLabel in valueData["changes"]) {
                         // Find all the label elements on the page.
                         var labelElements = document.querySelectorAll("label");
@@ -73,6 +82,34 @@
             });
         }
       });
+
+      // Define the event handler function.
+      function buttonClickHandler(event) {
+        // Check if localforageUserServiceChanges has any key-value pairs
+        localforageUserServiceChanges.length().then(function (numberOfKeys) {
+          if (numberOfKeys > 0) {
+            localforageUserServiceChanges.iterate(function (
+              valueData,
+              key,
+              iterationNumber
+            ) {
+              var url = window.location.href;
+              if (key == url.match(/\/(\d+)\/edit$/)[1]) {
+                localforageUserServiceChanges
+                  .removeItem(key)
+                  .then(function () {
+                    // The key-value pair has been removed.
+                    console.log("Removed data changes");
+                  })
+                  .catch(function (err) {
+                    // Handle any errors that occur during removal.
+                    console.error("Error removing data changes");
+                  });
+              }
+            });
+          }
+        });
+      }
     },
   };
 })(jQuery, Drupal, drupalSettings, localforage);
