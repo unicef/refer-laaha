00f6f3a80473da5df7c1aef4d92b0359b624b973
Hari Venu V <hari.venu@qed42.com>
2023-11-04 00:55:58 +0530

    Development

--- docroot/modules/custom/erpw_in_app_notification/src/Plugin/QueueWorker/NotificationProcessQueueWorker.php
+++ docroot/modules/custom/erpw_in_app_notification/src/Plugin/QueueWorker/NotificationProcessQueueWorker.php
@@ -3,6 +3,10 @@
 namespace Drupal\erpw_in_app_notification\Plugin\QueueWorker;
 
 use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\Query\Sql\QueryFactory;
 
 /**
  * Notification Process Queue Worker.
@@ -13,14 +17,50 @@ use Drupal\Core\Queue\QueueWorkerBase;
  *   cron = {"time" = 60}
  * )
  */
-class NotificationProcessQueueWorker extends QueueWorkerBase {
+class NotificationProcessQueueWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityQuery;
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructure for NotificationProcessQueueWorker.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueryFactory $entity_query, EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityQuery = $entity_query;
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.query'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Process item function.
    */
-  public function processItem($notification) {
-    // Process Item.
-    // @todo
+  public function processItem($id) {
+
   }
 
 }
