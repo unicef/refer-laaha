74402425826567cc87c4fcd2ae67980e053e65fb
Hari Venu V <hari.venu@qed42.com>
2023-11-09 19:19:34 +0530

    Development

--- docroot/modules/custom/erpw_in_app_notification/js/notification-bell.js
+++ docroot/modules/custom/erpw_in_app_notification/js/notification-bell.js
@@ -1,13 +1,21 @@
 (function ($, Drupal, drupalSettings) {
     Drupal.behaviors.notificationHelper = {
-			attach: function (context, settings) {
-			  // TODO
-			},
-		};
-})(jQuery, Drupal, drupalSettings);
+		attach: function (context, settings) {
+		// Set the interval time in milliseconds (e.g., 5000ms for 5 seconds).
+		var intervalTime = 60000;
 
-let count = 0;
-function increaseCount() {
-	count++;
-	document.getElementById('notificationCount').innerText = count;
-}
+		// Define the AJAX call function to be executed at the specified interval.
+		setInterval(function () {
+			// Perform your AJAX call here.
+			$.ajax({
+			url: 'ajax/getnotificaitoncount', // Replace with your custom AJAX path.
+			type: 'GET', // Use the appropriate HTTP method.
+			dataType: 'json',
+			success: function (response) {
+				document.getElementById('notificationCount').innerText = response.count;
+			},
+			});
+		}, intervalTime);
+		},
+	};
+})(jQuery, Drupal, drupalSettings);
