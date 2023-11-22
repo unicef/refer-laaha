d4acb7428973d43b07f0816d33c6ff9bbac60b32
Megha Sharma <megha.sharma@qed42.com>
2023-11-22 17:14:17 +0530

    In-app notifiication icon animation.

--- docroot/modules/custom/erpw_in_app_notification/js/notification-bell.js
+++ docroot/modules/custom/erpw_in_app_notification/js/notification-bell.js
@@ -2,20 +2,34 @@
     Drupal.behaviors.notificationHelper = {
 		attach: function (context, settings) {
 		// Set the interval time in milliseconds (e.g., 5000ms for 5 seconds).
-		var intervalTime = 60000;
+    var intervalTime = 60000;
+    var bellIconImg = document.querySelector('.notification-bell-icon');
+    var notificationNum = document.getElementById('notificationCount').innerText;
+    if (notificationNum > 0) {
+      bellIconImg.classList.add('scaled');
+    } else {
+      bellIconImg.classList.remove('scaled');
+    }
 
-		// Define the AJAX call function to be executed at the specified interval.
-		setInterval(function () {
-			// Perform your AJAX call here.
-			$.ajax({
-			url: 'ajax/getnotificaitoncount', // Replace with your custom AJAX path.
-			type: 'GET', // Use the appropriate HTTP method.
-			dataType: 'json',
-			success: function (response) {
-				document.getElementById('notificationCount').innerText = response.count;
-			},
-			});
-		}, intervalTime);
-		},
-	};
+    // Define the AJAX call function to be executed at the specified interval.
+    setInterval(function () {
+      // Perform your AJAX call here.
+      $.ajax({
+      url: 'ajax/getnotificaitoncount', // Replace with your custom AJAX path.
+      type: 'GET', // Use the appropriate HTTP method.
+      dataType: 'json',
+      success: function (response) {
+        var bellIcon = document.querySelector('.notification-bell-icon');
+        var notificationValue = response.count;
+        document.getElementById('notificationCount').innerText = notificationValue;
+        if (notificationValue > 0) {
+          bellIcon.classList.add('scaled');
+        } else {
+          bellIcon.classList.remove('scaled');
+        }
+      },
+      });
+    }, intervalTime);
+    },
+  };
 })(jQuery, Drupal, drupalSettings);
