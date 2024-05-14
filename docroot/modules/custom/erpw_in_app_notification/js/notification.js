283f52446aa5f7f60a507cc8935326dca5da30e2
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2024-05-14 16:26:01 +0530

    lint fixes for all custom modules except erpw_webform module is done.

--- docroot/modules/custom/erpw_in_app_notification/js/notification.js
+++ docroot/modules/custom/erpw_in_app_notification/js/notification.js
@@ -1,22 +1,21 @@
 (function ($, Drupal, drupalSettings) {
-    Drupal.behaviors.notificationHelper = {
-			attach: function (context, settings) {
-			  // TODO
-			},
-		};
+  Drupal.behaviors.notificationHelper = {
+    attach(context, settings) {
+      // TODO
+    },
+  };
 })(jQuery, Drupal, drupalSettings);
 
-
 function openTab(evt, cityName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(cityName).style.display = "block";
-	evt.currentTarget.className += " active";
+  let i;
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
